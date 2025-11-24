8bfd3e1d8356ef33418cfd4d41d4e32155c115da
ChristineTauy <chrosales07@gmail.com>
2025-11-25 05:29:32 +0800

    Feat: Add Index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Index from './pages/Index';
+import { Toaster as Sonner } from "@/components/ui/sonner";
 
 function App() {
 
   return (
     <>
-      
+    <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
