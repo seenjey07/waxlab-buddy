002c4eb5e1eeeabac09e58a0ed0ba540bb050360
ChristineTauy <chrosales07@gmail.com>
2025-11-25 06:35:53 +0800

    Feat: Add WelcomePage

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
+import WelcomePage from "@/components/WelcomePage";
 
 
 const Index = () => {
+    const [step, setStep] = useState(1);
+    const [waxWeight, setWaxWeight] = useState(0);
+    const [unit, setUnit] = useState("g");
+
+    const handleWelcomeNext = (weight: number, selectedUnit: string) => {
+        setWaxWeight(weight);
+        setUnit(selectedUnit);
+        setStep(2);
+      };
+
   return (
-    <div>Index</div>
+    <>
+      {step === 1 && <WelcomePage onNext={handleWelcomeNext} />}
+      {step === 2 && <div>Results: wax weight {waxWeight} {unit}</div>} {/* Placeholder muna pero next screen is for Stearic Acid weight input */}
+
+    </>
   )
 }
 
