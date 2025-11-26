fabfb715c77e1303eaad75fcb321bc0d1a79bcae
ChristineTauy <chrosales07@gmail.com>
2025-11-26 22:20:13 +0800

    Refactor: Replace strings with WeightUnit type in props

--- src/types/props.ts
+++ src/types/props.ts
@@ -1,27 +1,29 @@
+import type { WeightUnit } from "./index";
+
 export interface UnitToggleProps {
-    unit: string;
-    onUnitChange: (unit: string) => void;
-  }
+  unit: WeightUnit;
+  onUnitChange: (unit: WeightUnit) => void;
+}
 
-  export interface ProgressIndicatorProps {
-    currentStep: number;
-    totalSteps: number;
-  }
+export interface ProgressIndicatorProps {
+  currentStep: number;
+  totalSteps: number;
+}
 
-  export interface StearicAcidPageProps {
-    onNext: (percentage: number) => void;
-    onBack: () => void;
-  }
+export interface StearicAcidPageProps {
+  onNext: (percentage: number) => void;
+  onBack: () => void;
+}
 
-  export interface FragranceOilPageProps {
-    onCalculate: (percentage: number) => void;
-    onBack: () => void;
-  }
+export interface FragranceOilPageProps {
+  onCalculate: (percentage: number) => void;
+  onBack: () => void;
+}
 
-  export interface ResultsPageProps {
-    waxWeight: number;
-    unit: string;
-    saPercentage: number;
-    foPercentage: number;
-    onReset: () => void;
-  }
+export interface ResultsPageProps {
+  waxWeight: number;
+  unit: WeightUnit;
+  saPercentage: number;
+  foPercentage: number;
+  onReset: () => void;
+}
