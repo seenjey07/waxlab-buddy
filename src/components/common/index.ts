86685996da1307080627c4875b6d4ed55f433742
ChristineTauy <chrosales07@gmail.com>
2025-11-26 23:26:23 +0800

    Chore: Export ErrorMessage from common components

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,3 +2,4 @@ export { PageLayout } from "./PageLayout";
 export { InfoCard } from "./InfoCard";
 export { PageHeader } from "./PageHeader";
 export { ResultCard } from "./ResultCard";
+export { ErrorMessage } from "./ErrorMessage";
