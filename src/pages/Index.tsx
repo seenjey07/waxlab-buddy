c2efb5b615bf4af1e17a58a182158ca6b2041a84
ChristineTauy <chrosales07@gmail.com>
2025-11-26 20:29:18 +0800

    Feat: Add StearicAcidPage component and create handleSaNext function

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import WelcomePage from "@/components/WelcomePage";
+import StearicAcidPage from "@/components/StearicAcidPage";
 
 
 const Index = () => {
     const [step, setStep] = useState(1);
     const [waxWeight, setWaxWeight] = useState(0);
     const [unit, setUnit] = useState("g");
+    const [saPercentage, setSaPercentage] = useState(0);
 
     const handleWelcomeNext = (weight: number, selectedUnit: string) => {
         setWaxWeight(weight);
@@ -13,10 +15,17 @@ const Index = () => {
         setStep(2);
       };
 
+      const handleSaNext = (percentage: number) => {
+        setSaPercentage(percentage);
+        setStep(3);
+      };
+
   return (
     <>
       {step === 1 && <WelcomePage onNext={handleWelcomeNext} />}
-      {step === 2 && <div>Results: wax weight {waxWeight} {unit}</div>} {/* Placeholder muna pero next screen is for Stearic Acid weight input */}
+      {step === 2 && <StearicAcidPage onNext={handleSaNext} onBack={() => setStep(1)} />} 
+      {step === 3 && <div>Results: wax weight {waxWeight} {unit}, sa percentage {saPercentage}%</div>}
+      {/* Placeholder muna pero next screen is for Stearic Acid weight input */}
 
     </>
   )
